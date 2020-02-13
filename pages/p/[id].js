ae766b0e63e43092b5d7ceac91e8d4b922014764
Rodrigo Castro <rodrigocastro@iMac.local>
2020-02-13 06:32:37 +0100

    Styling Components - Styles and Nested Components

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -11,7 +11,7 @@ const Post = props => (
 
 Post.getInitialProps = async function(context) {
     const { id } = context.query;
-    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const res = await fetch(`http://api.tvmaze.com/shows/${id}`);
     const show = await res.json();
 
     console.log(`Fetched show: ${show.name}`);
