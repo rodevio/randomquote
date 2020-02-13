4d448c09a7743fda05ff9ea9deb5ca043ec18f31
Rodrigo Castro <rodrigocastro@iMac.local>
2020-02-13 06:36:47 +0100

    Styling Components - No Effect for Nested Component

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,30 @@ return [
 ];
 }
 
+// we must style the child component directly (helps to manage styles for bigges apps)
+
 const PostLink = ({ post }) => (
 <li>
     <Link href="/p/[id]" as={`/p/${post.id}`}>
     <a>{post.title}</a>
     </Link>
+
+    <style jsx>{`
+    li {
+        list-style: none;
+        margin: 5px 0;
+    }
+
+    a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+    }
+
+    a:hover {
+        opacity: 0.6;
+    }
+    `}</style>
 </li>
 );
 
